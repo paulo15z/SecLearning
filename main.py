fc2ca989cf9847b99a3c8f97c5c1de020ab3b634
paulo15z <paulociee123@gmail.com>
2025-03-19 21:13:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-print("BORA!")
-
-import pandas as pd #•	Carregue o dataset usando a biblioteca Pandas em Python:
-
-df = pd.read_csv('C:\DEPOSITO\MachineLearningCSV') 
-                 
-#•	Trate valores ausentes preenchendo-os com a mediana ou removendo linhas, se necessário:
-df.fillna(df.median(), inplace=True)
-
-#•	Converta features categóricas, como protocolo, para valores numéricos usando label encoding:
-from sklearn import preprocessing
-
-label_encoder = preprocessing.LabelEncoder()
-
-df["Protocol"] = label_encoder.fit_transform(df["Protocol"])
-
-
+print("calmo")
 
 

9dd3e9b5e3cb8d73702bc9819d7802e8cfb819f0
paulo15z <paulociee123@gmail.com>
2025-04-03 14:33:57 -0300

    primeiro teste de sniff

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Modelo de verificação de tráfego malicioso
 
     A ideia é criar algo caseiro e deixar rodando 24/7 no servidor de casa.
+
+
+    Dados coletados do repositório https://github.com/stamparm/ipsum
+
+Check simples de tráfego. 
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-# imports
-import numpy as np
+from scapy.all import sniff, IP 
 
-# estrutura principal
+#puxar a lista de https://github.com/stamparm/ipsum
 
-#algoritmo de detecção
+def load_blacklist(filename):
+    try:
+        with open(filename, "r") as file:
+            return set(line.strip() for line in file if line.strip())
+    except FileNotFoundError:
+        print("Erro: Arquivo de BlackList não encontrado")
+        return set()
+    
+blacklist = "lista.txt"
+malicious_ips = load_blacklist(blacklist)
 
-#algoritmo de resposta a incidente
+def check_packet(packet):
+    print(packet.summary())
+    if IP in packet:
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
 
-#interface?
+        if src_ip in malicious_ips or dst_ip in malicious_ips:
+            print(f"ALERTA! Tráfego detectado de/para IP malicioso: {src_ip} -> {dst_ip}")
+
+#INICIA A CAPTURA
+sniff(filter="ip", prn=check_packet, store=False, iface="eth0")
